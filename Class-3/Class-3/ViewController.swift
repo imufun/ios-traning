422ebd43bab9d0dc56fc493571a94ee1c2c6fd23
Brisk <brisk@Brisks-iMac.local>
2019-01-18 11:02:26 +0600

    init

---
+++ Class-3/Class-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class-3
+//
+//  Created by Brisk on 1/18/19.
+//  Copyright © 2019 Brisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib. s
+    }
+  
+
+}
+
---
+++ TabBarView/TabBarView/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabBarView
+//
+//  Created by Brisk on 1/18/19.
+//  Copyright © 2019 Brisk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
