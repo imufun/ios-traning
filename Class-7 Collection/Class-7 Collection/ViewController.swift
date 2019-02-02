2c5a6f761215c18326433b7c544be8982594fe55
Brisk <brisk@Hasibuls-Mac.local>
2019-02-01 21:48:44 -0800

    class 7

---
+++ Class-7 Collection/Class-7 Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Class-7 Collection
+//
+//  Created by Brisk on 2/1/19.
+//  Copyright © 2019 Brisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
